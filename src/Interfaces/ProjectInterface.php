7107e200b91d106d1b1bb6b9243a9e0191708bba
713uk13m <713uk13m@713uk13ms-MacBook-Pro.local>
2018-12-08 18:10:04 +0700

    Add zend escape

--- src/Interfaces/ProjectInterface.php
+++ src/Interfaces/ProjectInterface.php
@@ -18,7 +18,7 @@ namespace nguyenanhung\MySecurity\Interfaces;
  */
 interface ProjectInterface
 {
-    const VERSION       = '0.1.1';
+    const VERSION       = '0.1.2';
     const USE_BENCHMARK = TRUE;
 
     /**
